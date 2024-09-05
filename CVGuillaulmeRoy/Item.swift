48509808ef38d4f17257a5e3bbd89cf2e63223cf
GuiRoy232319 <64890072+GuiRoy232319@users.noreply.github.com>
2024-09-05 11:05:17 +0200

    Initial Commit

---
+++ CVGuillaulmeRoy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CVGuillaulmeRoy
+//
+//  Created by guill roy on 05/09/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CVGuillaulmeRoyTests/CVGuillaulmeRoyTests.swift
@@ -0,0 +1,16 @@
+//
+//  CVGuillaulmeRoyTests.swift
+//  CVGuillaulmeRoyTests
+//
+//  Created by guill roy on 05/09/2024.
+//
+
+import Testing
+
+struct CVGuillaulmeRoyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
